883e30f63a344347c1dff00d936f7ea2ce31425e
tapader13 <minhajtapader0@gmail.com>
2023-10-13 00:31:36 -0400

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'build', // Make sure it's set to 'build'
-  },
+  base: '/As_one_react/',
 });
